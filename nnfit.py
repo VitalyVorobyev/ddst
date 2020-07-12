9cf87cc9e33ff6ced70a5dbfa381dc590b84e798
Vitaly Vorobyev <vitaly.vorobyev@cern.ch>
2020-07-12 13:40:45 +0700

    nnfit: first attempt

--- nnfit.py
+++ nnfit.py
@@ -1,41 +1,92 @@
 #! /usr/bin/env python
 
-import tensorflow as tf
-from tensorflow import keras as k
+import jax
+import jax.numpy as np
+import jax.random as rjax
+import flax
+from flax import nn, optim, serialization
+
+from lib.params import mdn, mdstp, mpip
 
 """ Train NN to learn 3D or 5D pdf """
 
-def umaxlh_loss(model, input_, norm_sample):
-    """ loss function for the unbinned maximum likelihood fit """
-    return -2.*tf.reduce_sum(
-        tf.math.log(
-            model(input_) / tf.reduce_sum(model(norm_sample))
-        )
-    )
+class NN(nn.Module):
+    def apply(self, x):
+        x = nn.Dense(x, features=32)
+        x = nn.sigmoid(x)
+        x = nn.Dense(x, features=32)
+        x = nn.sigmoid(x)
+        x = nn.Dense(x, features=1)
+        return nn.sigmoid(x)
 
 
-def uniform_sample(space, n):
-    """ Generates uniform sample in a rectangular space """
-    return tf.stack([
-        tf.random.uniform([n], lo, hi) for lo, hi in space
-    ], axis=1)
+@flax.struct.dataclass
+class TrainState:
+    optimizer: optim.Optimizer
 
 
-def model(input_, space, norm_size):
+def do_fit(data, steps=100):
     """ """
-    # Create an optimizer.
-
-    model = k.Sequential(
-        k.layers.Dense(64, activation='relu', input_shape=(len(space))),
-        k.layers.Dense(32, activation='relu'),
-        k.layers.Dense(1, activation='sigmoid')
+    norm_sample_size = 10**6
+    norm_space = (
+        (-2.5, 15),  # energy range (MeV)
+        (0, 22),  # T(DD) range (MeV)
+        (2.004, 2.026),  # m(Dpi) range (GeV)
     )
 
-    loss_fn = lambda: umaxlh_loss(model, input_, uniform_sample(space, norm_size))
-    var_list_fn = lambda: model.trainable_weights
+    _, initial_params = NN.init(rjax.PRNGKey(0), data)
+    print('Model initialized:')
+    print(jax.tree_map(np.shape, initial_params))
 
-    # opt = k.optimizers.SGD(learning_rate=0.1)
-    # loss_fn = lambda: tf.keras.losses.mse(model(input), output)
-    # var_list_fn = lambda: model.trainable_weights
-    # for input, output in data:
-    #     opt.minimize(loss_fn, var_list_fn)
+    rng = rjax.PRNGKey(10)
+    rng, key1, key2, key3 = rjax.split(rng, 4)
+    keys = [key1, key2, key3]
+    norm_sample = np.column_stack([
+        rjax.uniform(rjax.PRNGKey(key[0]), (norm_sample_size,), minval=lo, maxval=hi)\
+            for key, (lo, hi) in zip(keys, norm_space)
+    ])
+
+    def loglh_loss(model):
+        """ loss function for the unbinned maximum likelihood fit """
+        return -np.sum(np.log(model(data))) +\
+            data.shape[0] * np.log(np.sum(model(norm_sample)))
+
+    model = nn.Model(NN, initial_params)
+    adam = optim.Adam(learning_rate=0.03)
+    optimizer = adam.create(model)
+
+    for i in range(steps):
+        loss, grad = jax.value_and_grad(loglh_loss)(optimizer.target)
+        optimizer = optimizer.apply_gradient(grad)
+        print(f'{i}/{steps}: loss: {loss:.3f}')
+
+
+    with open('nn_model_3d.dat', 'wb') as ofile:
+        state = TrainState(optimizer=optimizer)
+        data = flax.serialization.to_bytes(state)
+        print(f'Model serialized, num bytes: {len(data)}')
+        ofile.write(data)
+
+
+def get_vars(data):
+    """ """
+    e, mddsq, md1pisq = [data[:,i] for i in range(3)]
+    s = (e + mdn + mdstp)**2
+    md2pisq = s + 2*mdn**2 + mpip**2 - mddsq - md1pisq
+    tdd = (np.sqrt(mddsq) - 2*mdn)*10**3
+    md1pi = np.sqrt(md1pisq)
+    return (e, tdd, md1pi, mddsq, md1pisq, md2pisq)
+
+
+def main():
+    smc = np.load('mc_ddpip_3d_smeared.npy')
+    print(smc.shape)
+
+    data = np.column_stack(get_vars(smc)[:3])
+    print(data.shape)
+
+    do_fit(data, 100)
+
+
+if __name__ == '__main__':
+    main()
--- runmc.py
+++ runmc.py
@@ -5,6 +5,7 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
 
 from lib.params import gs, gt, mdn, mdstp, mpip
 from lib.toymc import MCProducer
@@ -35,9 +36,9 @@ def make_space_5d(erng, tddmax, mdpirng, gsre, gsim):
         ((gsim[0]*10**-3, gsim[1]*10**-3)), # gsim range
     )
 
-def gen_ddpip(space, chunks):
+def gen_ddpip(space, chunks, igs=gs, igt=gt):
     """ Main MC driver """
-    pdf = DnDnPip(gs, gt)
+    pdf = DnDnPip(igs, igt)
 
     driver = MCProducer(pdf.pdf, space)
     data = driver(chunks=chunks)
@@ -45,11 +46,11 @@ def gen_ddpip(space, chunks):
     if data.shape[1] == 5:
         data[:,3:] *= 10**3
 
-    np.save(f'mc_ddpip_{data.shape[1]}d', data)
+    np.save(f'mc_ddpip_{data.shape[1]}d_gs{igs.real*10**3:.2f}_{igs.imag*10**3:.2f}_ch{chunks}', data)
 
-def gen_ddpip_3d(ranges, chunks):
+def gen_ddpip_3d(ranges, chunks, igs=gs, igt=gt):
     """ """
-    gen_ddpip(make_space_3d(*ranges[:3]), chunks)
+    gen_ddpip(make_space_3d(*ranges[:3]), chunks, igs, igt)
 
 
 def gen_ddpip_5d(ranges, chunks):
@@ -74,20 +75,43 @@ def phsp_plot(ranges):
     plt.contourf(-yv-xv, yv, z)
     plt.show()
 
+
+def generate_3d_on_grid(chunks):
+    """ """
+    gsre = np.array([35, 36, 37, 38,39, 40, 41, 42, 43, 44, 45, 46]) * 10**-3
+    gsim = np.array([1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]) * 10**-3
+
+    for re, im in product(gsre, gsim):
+        print(f'generating {re*10**3:.2f} + i{im*10**3:.2f} ...')
+        gen_ddpip_3d(ranges, chunks=chunks,
+            igs=(re+1j*im),
+            igt=(25+1j*im),
+        )
+
 if __name__ == '__main__':
     try:
         ranges = (
-            [-2.5, 15],  # energy
-            22,  # Tdd max
-            [2.004, 2.026],  # m(D0 pi+)
+            [-2.5, 10],  # energy
+            10,  # Tdd max
+            [2.005, 2.016],  # m(D0 pi+)
             [30., 40.],  # gs.real
             [1.3, 1.7],   # gs.imag
         )
 
         phsp_plot(ranges)
         chunks = int(sys.argv[1])
-        # gen_ddpip_3d(ranges, chunks=chunks)
-        gen_ddpip_5d(ranges, chunks=chunks)
+        if len(sys.argv) == 4:
+            gsre = float(sys.argv[2]) * 10**-3
+            gsim = float(sys.argv[3]) * 10**-3
+        else:
+            gsre, gsim = gs.real, gs.imag
+
+        gen_ddpip_3d(ranges, chunks=chunks,
+            igs=(gsre + 1j*gsim),
+            igt=( 25. + 1j*gsim)
+        )
+
+        # gen_ddpip_5d(ranges, chunks=chunks)
 
     except IndexError:
-        print(f'Usage: ./runtoymc [chunks]')
+        print(f'Usage: ./runtoymc [chunks] [[real(gs)] [imag(gs)]]')
